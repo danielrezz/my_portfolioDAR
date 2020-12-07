30659c73ce2d017f7ad8b235a9292521b991b139
Daniel Reza <daniel.reza1007@gmail.com>
2020-12-06 21:11:22 -0600

    adding up arrow

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-var glow = document.querySelector(".hitext");
-setInterval(function(){
-    glow.toggleClass('hitext');
-}, 1000);
+var scrollBtn = document.getElementById("scrollBtn");
+var text = document.querySelector("footer");
+
+window.scroll = function() {
+    if (document.documentElement.scrollTop > 100) {
+      scrollBtn.addClass('show');
+    } else {
+      scrollBtn.removeClass('show');
+    }
+  };
+
+scrollBtn.onclick = function(e) {
+    e.preventDefault();
+    if (document.documentElement.scrollTop > 0) {
+        document.documentElement.scrollTop = 0;
+    };
+};
