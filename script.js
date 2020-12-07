b90be1e55060cd201579e721be817a899945a950
Daniel Reza <daniel.reza1007@gmail.com>
2020-12-07 17:49:56 -0600

    adding some media query

--- script.js
+++ script.js
@@ -15,11 +15,3 @@ scrollBtn.onclick = function(e) {
         document.documentElement.scrollTop = 0;
     };
 };
-
-var resumeImage = document.querySelector("#resumeImage");
-var resumeButton = document.querySelector(".resumeButton");
-
-resumeButton.onclick = function(e) {
-  e.preventDefault();
-  resumeImage.removeAttribute("style");
-}
