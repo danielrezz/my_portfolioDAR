c229f0d717977a52d100217580eff678d46fb0f2
Daniel Reza <daniel.reza1007@gmail.com>
2020-12-30 18:01:52 -0600

    adding dropdown menu

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ scrollBtn.onclick = function(e) {
         document.documentElement.scrollTop = 0;
     };
 };
+
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+
+menuBtn.addEventListener("click", () => {
+  if(!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
