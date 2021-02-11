cfc892de9fca65e38ec6b408d249cadb3ecbcdb9
Daniel Reza <daniel.reza1007@gmail.com>
2021-02-10 23:24:44 -0600

    added functionality to contact form

--- script.js
+++ script.js
@@ -28,3 +28,20 @@ menuBtn.addEventListener("click", () => {
     menuOpen = false;
   }
 });
+
+// function for emailjs
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('service_dareza', 'template_dareza', this)
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+}
